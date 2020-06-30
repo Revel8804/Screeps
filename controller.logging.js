416a025658146bbb98209bc587aa67d517fe54fa
Kenny Bell <kenny@revel.local>
2020-06-30 14:45:41 -0500

    cleaned up logging controller

--- controller.logging.js
+++ controller.logging.js
@@ -1,7 +1,16 @@
 var loggingController = {
     /** @param {Room} room */
     run: function(room) {
+        if(Game.time % 20 == 0) {
+            var harvest = _.filter(Game.creeps, (creep) => creep.memory.role == 'harvest');
+            console.log('Harvesters: ' + harvest.length);
+            var upgrade = _.filter(Game.creeps, (creep) => creep.memory.role == 'upgrade');
+            console.log('Upgraders: ' + upgrade.length);
+            var build = _.filter(Game.creeps, (creep) => creep.memory.role == 'build');
+            console.log('Builders: ' + build.length);
+        }
+        console.log('****************************');
     }
-}
+};
+
     module.exports = loggingController;
-    
