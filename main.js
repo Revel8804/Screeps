6ab9bc1a04acf5130864ecda8abcb1cd8e1f29b4
Kenny Bell <kenny@revel.local>
2020-06-30 10:37:37 -0500

    added upgrade role and added code for upgrade and lazy

--- main.js
+++ main.js
@@ -1,9 +1,17 @@
 var roleHarvest = require('role.harvest');
+var roleUpgrade = require('role.upgrade');
+var lazy = require('lazy');
 
 module.exports.loop = function () {
 
     for(var name in Game.creeps) {
         var creep = Game.creeps[name];
-        roleHarvest.run(creep);
+        if(creep.memory.role == 'harvest') {
+            roleHarvest.run(creep);
+            lazy.run(creep);
+        }
+        if(creep.memory.role == 'upgrade') {
+            roleUpgrade.run(creep);
+        }
     }
 }
