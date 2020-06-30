b1695d03096c46d5aa8a6cd7e0598f0a145b6ef1
Kenny Bell <kenny@revel.local>
2020-06-30 12:18:23 -0500

    moved everything to roomers

--- main.js
+++ main.js
@@ -1,33 +1,8 @@
-var roleHarvest = require('role.harvest');
-var roleUpgrade = require('role.upgrade');
-var roleBuild = require ('role.build');
-var lazy = require('lazy');
-var random = require('random');
-var spawn = require('spawn');
 var roomers = require('roomers');
 
 module.exports.loop = function () {
-
     _.forEach(Game.rooms, function(room) {
         if(room && room.controller && room.controller.my) {
             roomers.run(room);            
-        }
-    })
-
-    for(var name in Game.creeps) {
-        var creep = Game.creeps[name];
-        if(creep.memory.role == 'harvest') {
-            roleHarvest.run(creep);
-        }
-        if(creep.memory.role == 'upgrade') {
-            roleUpgrade.run(creep);
-        }
-        if(creep.memory.role == 'build') {
-            roleBuild.run(creep);
-        }
-        lazy.run(creep);
-        spawn.run(creep);
-    }
-        random.run();
-}
+            }})}
 // Game.spawns['Phred'].spawnCreep( [WORK, WORK, CARRY, MOVE], 'name', { memory: { role: '' } } );
--- roomers.js
+++ roomers.js
@@ -1,7 +1,30 @@
+var roleHarvest = require('role.harvest');
+var roleUpgrade = require('role.upgrade');
+var roleBuild = require ('role.build');
+var lazy = require('lazy');
+var random = require('random');
+var spawn = require('spawn');
+
 var roomers = {
     /** @param {Room} room **/
     run: function(room) {
         console.log(room);
+        for(var name in Game.creeps) {
+            var creep = Game.creeps[name];
+            if(creep.memory.role == 'harvest') {
+                roleHarvest.run(creep);
+            }
+            if(creep.memory.role == 'upgrade') {
+                roleUpgrade.run(creep);
+            }
+            if(creep.memory.role == 'build') {
+                roleBuild.run(creep);
+            }
+            lazy.run(creep);
+            spawn.run(creep);
+        }
+            random.run();
+    
     }
 };
 module.exports = roomers;
