8afcfe8d97f4bf40cd611152459f2d4e977acb96
Kenny Bell <kenny@revel.local>
2020-06-30 14:49:45 -0500

    added a space in logging

--- controller.logging.js
+++ controller.logging.js
@@ -3,11 +3,11 @@ var loggingController = {
     run: function(room) {
         if(Game.time % 20 == 0) {
             var harvest = _.filter(Game.creeps, (creep) => creep.memory.role == 'harvest');
-            console.log('Harvesters: ' + room + harvest.length);
+            console.log('Harvesters: ' + room + ' ' + harvest.length);
             var upgrade = _.filter(Game.creeps, (creep) => creep.memory.role == 'upgrade');
-            console.log('Upgraders: ' + room + upgrade.length);
+            console.log('Upgraders: ' + room + ' ' + upgrade.length);
             var build = _.filter(Game.creeps, (creep) => creep.memory.role == 'build');
-            console.log('Builders: ' + room + build.length);
+            console.log('Builders: ' + room + ' ' + build.length);
             console.log('****************************');
         }
     }
