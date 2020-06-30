6380044fbd6faf6e84d7e1b4ecb4395d37cb059f
Kenny Bell <kenny@revel.local>
2020-06-30 11:57:11 -0500

    added upgrade spawning

--- spawn.js
+++ spawn.js
@@ -1,13 +1,22 @@
 var spawn = {
     run: function() {
         var harvest = _.filter(Game.creeps, (creep) => creep.memory.role == 'harvest');
+        var upgrade = _.filter(Game.creeps, (creep) => creep.memory.role == 'upgrade');
+
     
         if(harvest.length < 5) {
             var newName = 'harvest' + Game.time;
-            console.log('Spawning new harvester: ' + newName);
+            console.log('Spawning new Harvester: ' + newName);
             Game.spawns['Phred'].spawnCreep([WORK,CARRY,MOVE], newName, 
                 {memory: {role: 'harvest'}});        
         }
+        if(upgrade.length < 5) {
+            var newName = 'upgrade' + Game.time;
+            console.log('Spawning new Upgrader: ' + newName);
+            Game.spawns['Phred'].spawnCreep([WORK,CARRY,MOVE], newName, 
+                {memory: {role: 'upgrade'}});        
+        }
+
         
         if(Game.spawns['Phred'].spawning) { 
             var spawningCreep = Game.creeps[Game.spawns['Phred'].spawning.name];
