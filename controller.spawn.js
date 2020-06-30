9507d9168c60c2c3bb18c6dc58126a94fbae1520
Kenny Bell <kenny@revel.local>
2020-06-30 14:25:10 -0500

    bigger spawns!

--- controller.spawn.js
+++ controller.spawn.js
@@ -36,29 +36,29 @@ var spawnController = {
         // Check to see if we need more creeps of each job
         var name = -1;
         if (!numberOfHarvest && (!numberOfMine || !numberOfTransport)) {
-            name=Game.spawns.Phred.createCreep([WORK, CARRY, MOVE],undefined, {role: 'harvest', working:false, target: undefined});
+            name=Game.spawns.Phred.createCreep([WORK, WORK, WORK, CARRY, CARRY, MOVE, MOVE, MOVE],undefined, {role: 'harvest', working:false, target: undefined});
         }
 
         if(name == -1 && numberOfHarvest < minNumberOfHarvest) {
-            name=Game.spawns.Phred.createCreep(getBody([WORK, CARRY, MOVE], room),undefined, {role: 'harvest', working:false, target: undefined});
+            name=Game.spawns.Phred.createCreep(getBody([WORK, WORK, WORK, CARRY, CARRY, MOVE, MOVE, MOVE], room),undefined, {role: 'harvest', working:false, target: undefined});
         }
         else if(name == -1 && numberOfMine < minNumberOfMine) {
-            name=Game.spawns.Phred.createCreep(getBody([WORK, CARRY, MOVE], room),undefined, {role: 'mine', working:false, target: undefined});
+            name=Game.spawns.Phred.createCreep(getBody([WORK, WORK, WORK, CARRY, CARRY, MOVE, MOVE, MOVE], room),undefined, {role: 'mine', working:false, target: undefined});
         }
         else if(name == -1 && numberOfTransport < minNumberOfTransport) {
             name=Game.spawns.Phred.createCreep(getBody([CARRY, CARRY, MOVE], room),undefined, {role: 'transport', working:false, target: undefined});
         }
         else if(name == -1 && numberOfUpgrade < minNumberOfUpgrade) {
-            name=Game.spawns.Phred.createCreep(getBody([WORK, CARRY, MOVE], room),undefined, {role: 'upgrade', working:false, target: undefined});
+            name=Game.spawns.Phred.createCreep(getBody([WORK, WORK, WORK, CARRY, CARRY, MOVE, MOVE, MOVE], room),undefined, {role: 'upgrade', working:false, target: undefined});
         }
         else if(name == -1 && numberOfBuild < minNumberOfBuild && room.find(FIND_CONSTRUCTION_SITES).length > 0) {
-            name=Game.spawns.Phred.createCreep(getBody([WORK, CARRY, MOVE], room),undefined, {role: 'build', working:false, target: undefined});
+            name=Game.spawns.Phred.createCreep(getBody([WORK, WORK, WORK, CARRY, CARRY, MOVE, MOVE, MOVE], room),undefined, {role: 'build', working:false, target: undefined});
         }
         else if(name == -1 && numberOfRepair < minNumberOfRepair) {
-            name=Game.spawns.Phred.createCreep(getBody([WORK, CARRY, MOVE], room),undefined, {role: 'repair', working:false, target: undefined});
+            name=Game.spawns.Phred.createCreep(getBody([WORK, WORK, WORK, CARRY, CARRY, MOVE, MOVE, MOVE], room),undefined, {role: 'repair', working:false, target: undefined});
         }
         else if(name == -1 && numberOfWall < minNumberOfWall) {
-            name=Game.spawns.Phred.createCreep(getBody([WORK, CARRY, MOVE], room),undefined, {role: 'wall', working:false, target: undefined});
+            name=Game.spawns.Phred.createCreep(getBody([WORK, WORK, WORK, CARRY, CARRY, MOVE, MOVE, MOVE], room),undefined, {role: 'wall', working:false, target: undefined});
         }
     
         if (!(name < 0)) {
