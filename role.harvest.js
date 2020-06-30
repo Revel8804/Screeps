886c73e29318ff3c5ac9981566c93167740de280
Kenny Bell <kenny@revel.local>
2020-06-30 11:16:09 -0500

    cleaned up harvest role

--- role.harvest.js
+++ role.harvest.js
@@ -18,10 +18,8 @@ var roleHarvest = {
             if(targets.length > 0) {
                 if(creep.transfer(targets[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
                     creep.moveTo(targets[0], {visualizePathStyle: {stroke: '#ffffff'}});
-                }
-            }
-        }
-	}
+                }}}
+    }
 };
 
 module.exports = roleHarvest;
