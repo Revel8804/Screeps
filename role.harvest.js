56d0528fd76639110f8337e8a31b19cf26a656d8
Kenny Bell <kenny@revel.local>
2020-06-30 10:15:57 -0500

    added harvester role module

---
+++ role.harvest.js
@@ -0,0 +1,19 @@
+var roleHarvest = {
+
+    /** @param {Creep} creep **/
+    run: function(creep) {
+	    if(creep.store.getFreeCapacity() > 0) {
+            var sources = creep.room.find(FIND_SOURCES);
+            if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
+                creep.moveTo(sources[0]);
+            }
+        }
+        else {
+            if(creep.transfer(Game.spawns['Phred'], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+                creep.moveTo(Game.spawns['Phred']);
+            }
+        }
+	}
+};
+
+module.exports = roleHarvester;
